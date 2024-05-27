04ea07993ce7a9a5b3a5a66a5cf13df8b128141f
Muhammad-Noman03 <nomireigns@gmail.com>
2024-05-28 01:36:02 +0500

    finalize the todlist app

--- todolist/index.js
+++ todolist/index.js
@@ -18,6 +18,12 @@ input.addEventListener('keydown', (e) => {
 
 });
 
+addBtm.addEventListener('click', () => {
+
+    addTask();
+    Display();
+})
+
 function Display() {
     let html = '';
 
@@ -25,15 +31,17 @@ function Display() {
 
     todo.forEach((value, index) => {
         html += `
-                <div id="Task${index}" class=" flex px-4 py-4 rounded-lg border-2 border-black">
+                 <div id="Task${index}" class=" flex p-2 rounded-lg border-2 border-black">
                     <input type="text" id="input" class="outline-none grow" placeholder="Enter the Task"
                         autocomplete="off" value="${value}" readonly>
                     <div id="editBtn"
-                        class=" flex items-center justify-center mr-2 w-[6rem] p-2 rounded-lg bg-slate-600 text-white cursor-pointer" data-id="task${index}">
+                        class=" flex items-center justify-center mr-2 w-[6rem] p-2 rounded-lg bg-slate-600 text-white cursor-pointer"
+                        data-id="task${index}" onClick="edit(event,${index})">
                         Edit Task
                     </div>
                     <div id="deleteBtn"
-                        class=" flex items-center p-2 rounded-lg w-[6rem] bg-slate-600 text-white cursor-pointer" data-id="task${index}" onClick = "deleteTask(${index})">
+                        class=" flex items-center p-3 rounded-lg  bg-slate-600 text-white cursor-pointer"
+                        data-id="task${index}" onClick="deleteTask(${index})">
                         Delete Task
                     </div>
                 </div>
@@ -50,10 +58,26 @@ function addTask() {
     input.value = '';
 }
 
-// function edit () { 
-//     let el = el.target;
+function edit(e, index) {
+    let el = e.target;
+    // console.log(e);  
 
-// }
+
+    let input = el.parentNode.children[0];
+    // console.log(input);
+
+    if (el.innerText === 'Edit Task') {
+        el.innerText = 'Save Task'
+        input.removeAttribute('readonly');
+    } else if (input.value) {
+        el.innerText = 'Edit Task'
+        input.setAttribute('readonly', 'readonly');
+        todo.splice(index, 1, input.value)
+    }
+
+}
+
+
 
 function deleteTask(index) {
     todo.splice(index, 1);
